8a0deeebbb2e87be37291a2bf8f8e51a3d23ab0f
r_zim <rick@invizix.tech>
2018-04-07 12:40:49 -0700

    adding tests

---
+++ PokerHand.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cards;
+
+namespace PokerHand.Tests
+{
+    [TestClass]
+    public class PokerHandControllerTests
+    {
+
+    }
+
+}
